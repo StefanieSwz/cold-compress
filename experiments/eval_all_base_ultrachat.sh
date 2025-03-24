510772fe7d9540816b87c171f83493879788dab2
StefanieSwz <st_schwarz@outlook.de>
2025-03-24 16:09:23 +0100

    more gpus

--- experiments/eval_all_base_ultrachat.sh
+++ experiments/eval_all_base_ultrachat.sh
@@ -3,10 +3,10 @@
 set -e
 
 MODELS=(
-	# "checkpoints/meta-llama/Llama-3.2-3B-Instruct/model.pth"
+	"checkpoints/meta-llama/Llama-3.2-3B-Instruct/model.pth"
 	"checkpoints/Qwen/Qwen2-1.5B-Instruct/model.pth"
 )
-NUM_SAMPLES=500
+NUM_SAMPLES=700
 CACHE_SIZES="0.75 0.5 0.25 0.1 0.05"
 TASKS="ultrachat"
 CACHE_CONFIGS="random l2 heavy_hitter recent_global fastgen hybrid"
@@ -19,6 +19,6 @@ for MODEL in ${MODELS[@]}; do
 		--tasks $TASKS \
 		--cache_sizes $CACHE_SIZES \
 		--num_samples $NUM_SAMPLES \
-		--num_gpus 2 \
+		--num_gpus 4 \
 		--add_full
 done
