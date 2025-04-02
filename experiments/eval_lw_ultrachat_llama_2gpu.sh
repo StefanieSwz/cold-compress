d6c783c4c9074ed83b83259649b090aaedd4d7cd
Stefanie Schwarz <121288237+StefanieSwz@users.noreply.github.com>
2025-04-02 11:45:14 +0200

    Update eval_lw_ultrachat_llama_2gpu.sh

--- experiments/eval_lw_ultrachat_llama_2gpu.sh
+++ experiments/eval_lw_ultrachat_llama_2gpu.sh
@@ -7,7 +7,7 @@ set -e
 # 	"checkpoints/Qwen/Qwen2-1.5B-Instruct/model.pth"
 # ) # in model version, just a reminder
 NUM_SAMPLES=700 # keep
-CACHE_SIZES="0.75 0.5 0.25 0.1 0.05" # keep
+CACHE_SIZES="0.5 0.25 0.1 0.05" # keep
 TASKS="ultrachat"
 LIGHTWEIGHT_MODEL_VERSIONS="v169 v170 v168 v167 v165 v164 v166 v162 v163 v161 v160" # Version defines the model, rest must fit to the model version
 
